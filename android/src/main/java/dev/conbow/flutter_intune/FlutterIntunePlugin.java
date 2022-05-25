61d9c0cfaed8962d77b43ae757238af6ff21f4d0
Connor Bowman <connor.bowman@lexisnexis.com>
2022-05-25 17:40:42 -0400

    Use Flutter activity for diagnostics

--- android/src/main/java/dev/conbow/flutter_intune/FlutterIntunePlugin.java
+++ android/src/main/java/dev/conbow/flutter_intune/FlutterIntunePlugin.java
@@ -1,31 +1,32 @@
 package dev.conbow.flutter_intune;
 
-import android.content.Context;
+import android.app.Activity;
 
 import androidx.annotation.NonNull;
 
 import com.microsoft.intune.mam.client.identity.MAMPolicyManager;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
 /** FlutterIntunePlugin */
-public class FlutterIntunePlugin implements FlutterPlugin, MethodCallHandler {
+public class FlutterIntunePlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
-  private Context mContext;
+  private Activity activity;
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
     channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_intune");
     channel.setMethodCallHandler(this);
-    mContext = flutterPluginBinding.getApplicationContext();
   }
 
   @Override
@@ -33,7 +34,7 @@ public class FlutterIntunePlugin implements FlutterPlugin, MethodCallHandler {
     if (call.method.equals("getPlatformVersion")) {
       result.success("Android " + android.os.Build.VERSION.RELEASE);
     } else if (call.method.equals("showDiagnostics")) {
-      MAMPolicyManager.showDiagnostics(mContext);
+      MAMPolicyManager.showDiagnostics(activity);
       result.success("");
     } else {
       result.notImplemented();
@@ -43,5 +44,24 @@ public class FlutterIntunePlugin implements FlutterPlugin, MethodCallHandler {
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
     channel.setMethodCallHandler(null);
+  }
+
+  @Override
+  public void onAttachedToActivity(ActivityPluginBinding activityPluginBinding) {
+    activity = activityPluginBinding.getActivity();
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding activityPluginBinding) {
+    activity = activityPluginBinding.getActivity();
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+    activity = null;
   }
 }
